84d62105e331d207a3da4105ca7fdd419e78ad3e
angelogeraci <ageraci.finance@gmail.com>
2025-04-17 11:15:16 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'meta-blue': '#1877F2',
+        'meta-light': '#E4F0F6',
+      },
+    },
+  },
+  plugins: [],
+}
